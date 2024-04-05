5a76a9bcd086a918a260c30fdf7e97f1646a2bc3
Chris Li <c58li@uwaterloo.ca>
2024-04-04 23:03:52 -0400

    Setup database and switch to prisma

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,5 +14,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    }
+  });
+
+  return { success: "User created!" };
 };
